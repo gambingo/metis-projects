cfbe2793e5ef8f389dcf4dc1751371d5ab96d220
Joseph Gambino <joe@gam.bingo>
2018-01-26 13:43:42 -0600

    test pickling subset

--- metis-05-kojak/python-scripts/run_on_AWS.py
+++ metis-05-kojak/python-scripts/run_on_AWS.py
@@ -16,64 +16,73 @@ filepath = '../data/North-Carolina-2014.geojson'
 with open(filepath) as f:
     data = json.load(f)
 data = data['features']
+subset = slice_up_a_state(data, long_east=-83)
 
 # To prevent recurion depth limit on AWS.
 sys.setrecursionlimit(10000)
 
 # Stage One
 k = [4/13, 4/13, 5/13]
+pop_error_limit = 0.005
 stage_one = redistricting(k=k, weights=[1, 1, 0], seed=42,
+                          pop_error_limit=pop_error_limit,
                           compactness_method = 'sum',
                           gif=False, n_jobs=-1, logging=True)
-save_pickle(stage_one, 'stage_one_')
-print('test pickle')
-
-stage_one.fit(data)
+stage_one.fit(subset)
 
 save_pickle(model, 'stage_one_')
-command = 'cp ../images/frames/* ../images/frames_stage_one'
-subprocess.call(command.split())
-print('Completed Stage One')
+print('Succesfully pickled model.')
 
-# Stage Two
-dst_pops = [dst.current_pop for dst in stage_one.districts]
-stage_two_dst = []
-for dst in stage_one.districts:
-    if dst.current_pop == max(dst_pops):
-        k = [2/5, 3/5]
-    else:
-        k=2
 
-    model = redistricting(k=k, weights=[1, 1, 0], seed=42,
-                          compactness_method = 'sum',
-                          gif=False, n_jobs=-1, logging=True)
-    model.fit(dst)
-    stage_two_dst += model.districts
-
-save_pickle(stage_two_dst, 'stage_two_dst_')
-command = 'cp ../images/frames/* ../images/frames_stage_two'
-subprocess.call(command.split())
-print('Completed Stage Two')
-
-# Stage Three
-dst_pops = [dst.current_pop for dst in stage_two_dst]
-stage_three_dst = []
-for dst in stage_two_dst:
-    if dst.current_pop == max(dst_pops):
-        k = 3
-    else:
-        k = 2
-    model = redistricting(k=k, weights=[1, 1, 0], seed=42,
-                          compactness_method = 'sum',
-                          gif=False, n_jobs=-1, logging=True)
-    model.fit(dst)
-    stage_three_dst += model.districts
-
-save_pickle(stage_three_dst, 'stage_three_dst')
-command = 'cp ../images/frames/* ../images/frames_stage_three'
-subprocess.call(command.split())
-print('Completed Stage Three')
-
-# Final Plot
-_, ax = rpl.initialize_plot(stage_three_dst)
-rpl.final_plot(ax, stage_three_dst, color_by_party=True)
+# command = 'cp ../images/frames/* ../images/frames_stage_one'
+# subprocess.call(command.split())
+# print('Completed Stage One')
+#
+# # Stage Two
+# dst_pops = [dst.current_pop for dst in stage_one.districts]
+# stage_two_dst = []
+# for dst in stage_one.districts:
+#     if dst.current_pop == max(dst_pops):
+#         k = [2/5, 3/5]
+#         pop_error_limit = 0.005
+#     else:
+#         k=2
+#         pop_error_limit = 0.005
+#
+#     model = redistricting(k=k, weights=[1, 1, 0], seed=42,
+#                           pop_error_limit=pop_error_limit,
+#                           compactness_method = 'sum',
+#                           gif=False, n_jobs=-1, logging=True)
+#     model.fit(dst)
+#     stage_two_dst += model.districts
+#
+# save_pickle(stage_two_dst, 'stage_two_dst_')
+# command = 'cp ../images/frames/* ../images/frames_stage_two'
+# subprocess.call(command.split())
+# print('Completed Stage Two')
+#
+# # Stage Three
+# dst_pops = [dst.current_pop for dst in stage_two_dst]
+# stage_three_dst = []
+# for dst in stage_two_dst:
+#     if dst.current_pop == max(dst_pops):
+#         k = 3
+#         pop_error_limit = 0.005
+#     else:
+#         k = 2
+#         pop_error_limit = 0.005
+#     model = redistricting(k=k, weights=[1, 1, 0], seed=42,
+#                           pop_error_limit=pop_error_limit,
+#                           compactness_method = 'sum',
+#                           gif=False, n_jobs=-1, logging=True)
+#     model.fit(dst)
+#     stage_three_dst += model.districts
+#
+# save_pickle(stage_three_dst, 'stage_three_dst')
+# command = 'cp ../images/frames/* ../images/frames_stage_three'
+# subprocess.call(command.split())
+# print('Completed Stage Three')
+#
+# # Final Plot
+# _, ax = rpl.initialize_plot(stage_three_dst)
+# rpl.final_plot(ax, stage_three_dst, color_by_party=True)
